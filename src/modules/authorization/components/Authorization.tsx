a5563b4db43fbca456bee10d1a6fe9950d2ff729
oriabkokattana <riabkoleksandr@kattana.io>
2024-10-01 00:48:58 +0300

    feat: remove biometry from auth screen

--- src/modules/authorization/components/Authorization.tsx
+++ src/modules/authorization/components/Authorization.tsx
@@ -1,35 +1,40 @@
-import { useEffect, useState } from 'react';
+import {
+  // useEffect,
+  useState,
+} from 'react';
 import { Flex, Heading, Section, SegmentedControl } from '@radix-ui/themes';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-import { BiometryManager, initBiometryManager } from '@telegram-apps/sdk-react';
-import Biometry from './Biometry';
+// import { BiometryManager, initBiometryManager } from '@telegram-apps/sdk-react';
+// import Biometry from './Biometry';
 import EmailPassword from './EmailPassword';
 import OAuth from './OAuth';
 
 type AuthMethod = 'Email&Password' | 'OAuth' | 'Biometry' | 'Wallet';
 
+const TABS = ['Email&Password', 'OAuth', 'Wallet'];
+
 const Authorization = () => {
   const [tab, setTab] = useState<AuthMethod>('Email&Password');
-  const [bm, setBm] = useState<BiometryManager>();
+  // const [bm, setBm] = useState<BiometryManager>();
 
-  useEffect(() => {
-    const [biometryManager] = initBiometryManager();
-    biometryManager.then((bm) => setBm(bm)).catch((e) => console.log(e));
-  }, []);
+  // useEffect(() => {
+  //   const [biometryManager] = initBiometryManager();
+  //   biometryManager.then((bm) => setBm(bm)).catch((e) => console.log(e));
+  // }, []);
 
-  const tabs = bm?.available
-    ? ['Email&Password', 'OAuth', 'Wallet', 'Biometry']
-    : ['Email&Password', 'OAuth', 'Wallet'];
+  // const tabs = bm?.available
+  //   ? ['Email&Password', 'OAuth', 'Wallet', 'Biometry']
+  //   : ['Email&Password', 'OAuth', 'Wallet'];
 
   return (
     <Flex width='100vw' height='100vh' px='4' py='4' direction='column'>
       <Flex gap='2' align='center' mb='2'>
-        <Heading>Sign in/up</Heading>
+        <Heading>Login/Sign up</Heading>
       </Flex>
       <Flex width='100%' my='auto' direction='column' gap='6'>
         <Flex justify='center'>
           <SegmentedControl.Root size='1' defaultValue='Email&Password'>
-            {tabs.map((item) => (
+            {TABS.map((item) => (
               <SegmentedControl.Item
                 key={item}
                 value={item}
@@ -42,7 +47,7 @@ const Authorization = () => {
         </Flex>
         {tab === 'Email&Password' && <EmailPassword />}
         {tab === 'OAuth' && <OAuth />}
-        {tab === 'Biometry' && <Biometry biometryManager={bm!} />}
+        {/* {tab === 'Biometry' && <Biometry biometryManager={bm!} />} */}
         {tab === 'Wallet' && (
           <Section py='6'>
             <Flex justify='center'>
